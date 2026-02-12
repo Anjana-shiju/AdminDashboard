3b98202946ecfed6edd63b93d870bc4d595e9d61
Anjana Shiju <anjanashiju28@gmail.com>
2026-02-12 15:52:44 +0530

    ediss

--- src/COMPONENTS/StatCard.js
+++ src/COMPONENTS/StatCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bed, Home, Tag, CalendarX, ChevronDown, Calendar, ArrowUpRight, TrendingUp, TrendingDown } from 'lucide-react';
+import { ChevronDown, Calendar, ArrowUpRight, TrendingUp, TrendingDown } from 'lucide-react';
 
 const PayingGuestIcon = ({ size = 20, color = "currentColor" }) => (
   <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
